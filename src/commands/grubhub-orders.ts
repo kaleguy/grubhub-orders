5d5c191dd5f1f02a469dfc458eaaa7b37a0ec74f
Joseph Orr <joe.orr@gmail.com>
2020-07-28 19:47:01 -0400

    dist

--- src/commands/grubhub-orders.ts
+++ src/commands/grubhub-orders.ts
@@ -1,14 +1,31 @@
 
 import { GluegunCommand } from 'gluegun'
-
+const lineReader = require('line-reader');
 
 const command: GluegunCommand = {
   name: 'grubhub-orders',
   run: async toolbox => {
 
+    // @ts-ignore
     const { system, print, filesystem, strings } = toolbox
-    print.info('Welcome to your CLI')
+    print.info('Extracting Data from All Mail')
 
+    const htmlLines: string[] = [];
+    lineReader.eachLine('./data/in/All Mail', function(line, last) {
+      console.log(line);
+
+      if (line.includes('<!DOCTYPE html>')) {
+        return false; // stop reading
+      }
+    });
+
+  },
+}
+
+module.exports = command
+
+
+/*
     // ...and be the CLI you wish to see in the world
     const awesome = strings.trim(await system.run('whoami'))
     const moreAwesome = strings.kebabCase(`${awesome} and a keyboard`)
@@ -19,7 +36,5 @@ const command: GluegunCommand = {
     print.info(`${print.checkmark} Citius`)
     print.warning(`${print.checkmark} Altius`)
     print.success(`${print.checkmark} Fortius`)
-  },
-}
 
-module.exports = command
+ */
